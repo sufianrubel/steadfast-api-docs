479fe2e231b709c430c6fe8d7a06c9fd83d531a1
Abu Sufian Rubel <rubel.nstu27@gmail.com>
2026-07-21 23:21:39 +0600

    Initial Docusaurus project

---
+++ sidebars.js
@@ -0,0 +1,55 @@
+// @ts-check
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+
+ @type {import('@docusaurus/plugin-content-docs').SidebarsConfig}
+ */
+const sidebars = {
+  tutorialSidebar: [
+    "intro",
+
+    "getting-started",
+
+    "authentication",
+
+    "quick-start",
+
+    {
+      type: "category",
+      label: "Orders",
+      items: [
+        "orders/create-order",
+        "orders/bulk-order",
+        "orders/delivery-status",
+      ],
+    },
+
+    {
+      type: "category",
+      label: "Returns",
+      items: [
+        "returns/create-return",
+      ],
+    },
+
+    {
+      type: "category",
+      label: "Payments",
+      items: [
+        "payments/balance",
+      ],
+    },
+  ],
+};
+
+export default sidebars;
